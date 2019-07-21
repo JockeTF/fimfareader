d1f9f588d50bc7a9f58b17624b5784b9623b3e3b
Joakim Soderlund <joakim.soderlund@gmail.com>
2019-07-21 17:13:43 +0000

    Add slightly interactive main function

--- src/error.rs
+++ src/error.rs
@@ -4,6 +4,7 @@
 pub enum Error {
     InvalidStory(),
     SourceError(&'static str),
+    UserError(&'static str),
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,30 @@ pub mod fetcher;
 pub mod parser;
 pub mod story;
 
-fn main() {
+use std::env::args;
+use std::time::Instant;
+
+use crate::error::{Error, Result};
+use crate::fetcher::Fetcher;
+
+fn main() -> Result<()> {
+    use Error::*;
+
+    let argv = args().collect::<Vec<String>>();
+
+    let path = match argv.len() {
+        2 => Ok(argv.get(1).unwrap()),
+        _ => Err(UserError("Usage: fimfareader <ARCHIVE>")),
+    }?;
+
     println!("Hellopaca, World!");
+
+    let start = Instant::now();
+    let fetcher = Fetcher::from(path)?;
+    let finish = Instant::now() - start;
+
+    println!("Finished loading in {} milliseconds.", finish.as_millis());
+    println!("The archive contains {} stories.", fetcher.iter().count());
+
+    Ok(())
 }
