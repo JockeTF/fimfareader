eecd2c73562ee6f6243a920d5b83f471d423caae
Joakim Soderlund <joakim.soderlund@gmail.com>
2019-09-07 17:14:00 +0200

    Use crossbeam for index parsing

--- src/archive/parser.rs
+++ src/archive/parser.rs
@@ -1,25 +1,34 @@
 //! Index parser.
 
 use std::io::BufRead;
-use std::sync::mpsc::{channel, Receiver};
 use std::thread::spawn;
 
 use serde::de::Error;
 use serde_json::error::Result;
 use serde_json::from_str;
 
+use crossbeam_channel::{bounded, Receiver};
+
 use super::story::Story;
 
 const TRIM: &[char] = &['"', ',', ' ', '\t', '\n', '\r'];
 
 pub fn parse(reader: impl BufRead) -> Result<Vec<Story>> {
-    let (tx, rx) = channel();
+    let mut wrappers = String::with_capacity(2);
+
+    let (tx, rx) = bounded(4096);
     let rx = spawn_parser(rx);
+    let rx = spawn_reducer(rx);
 
     for line in reader.lines() {
         let line = line.map_err(|e| match e {
             _ => Error::custom("Could not read line"),
         })?;
+
+        if line.len() == 1 {
+            wrappers.push_str(&line);
+            continue;
+        }
 
         if tx.send(line).is_ok() {
             continue;
@@ -34,28 +43,48 @@ pub fn parse(reader: impl BufRead) -> Result<Vec<Story>> {
 
     drop(tx);
 
+    if wrappers != "{}" {
+        return Err(Error::custom("Invalid file structure"));
+    }
+
     rx.recv().map_err(|e| match e {
         _ => Error::custom("Missing parser result"),
     })?
 }
 
-fn spawn_parser(stream: Receiver<String>) -> Receiver<Result<Vec<Story>>> {
-    let (tx, rx) = channel();
+fn spawn_parser(ix: Receiver<String>) -> Receiver<Result<Story>> {
+    let (tx, rx) = bounded(4096);
+
+    for _ in 0..2 {
+        let stream = ix.clone();
+        let result = tx.clone();
+
+        spawn(move || {
+            while let Ok(line) = stream.recv() {
+                match result.send(deserialize(line)) {
+                    Err(e) => return Err(e),
+                    Ok(()) => continue,
+                }
+            }
+
+            Ok(())
+        });
+    }
+
+    rx
+}
+
+fn spawn_reducer(ix: Receiver<Result<Story>>) -> Receiver<Result<Vec<Story>>> {
+    let (tx, rx) = bounded(1024);
 
     spawn(move || {
         let mut stories = Vec::with_capacity(250_000);
-        let mut wrappers = String::with_capacity(2);
 
-        while let Ok(line) = stream.recv() {
-            if line.len() == 1 {
-                wrappers.push_str(&line);
-                continue;
-            }
-
-            match deserialize(line) {
+        while let Ok(story) = ix.recv() {
+            match story {
                 Ok(story) => stories.push(story),
                 Err(e) => return tx.send(Err(e)),
-            };
+            }
         }
 
         let count = stories.len();
@@ -63,10 +92,6 @@ fn spawn_parser(stream: Receiver<String>) -> Receiver<Result<Vec<Story>>> {
         stories.sort_by_key(|story| story.id);
         stories.dedup_by_key(|story| story.id);
         stories.shrink_to_fit();
-
-        if wrappers != "{}" {
-            return tx.send(Err(Error::custom("Invalid file structure")));
-        }
 
         if count != stories.len() {
             return tx.send(Err(Error::custom("Found duplicate story")));
