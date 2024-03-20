217eb6240f619e6a81fa50487a6e72bd619991b6
Joakim Soderlund <joakim.soderlund@gmail.com>
2024-03-20 17:52:40 +0100

    Upgrade dependency Nom to v7

--- query/src/parser.rs
+++ query/src/parser.rs
@@ -1,11 +1,28 @@
 //! Query parser.
 
-use chrono::prelude::*;
+use chrono::DateTime;
+use chrono::Utc;
 
-use nom::character::complete::*;
+use nom::branch::alt;
+use nom::bytes::complete::escaped;
+use nom::bytes::complete::tag;
+use nom::character::complete::char;
+use nom::character::complete::none_of;
+use nom::character::complete::one_of;
+use nom::character::complete::space0;
+use nom::combinator::eof;
+use nom::combinator::map;
+use nom::combinator::value;
+use nom::error::Error as NomError;
 use nom::error::ErrorKind as NomErrorKind;
-use nom::sequence::*;
-use nom::*;
+use nom::multi::separated_list1;
+use nom::sequence::delimited;
+use nom::sequence::preceded;
+use nom::sequence::terminated;
+use nom::sequence::tuple;
+use nom::Err as NomErr;
+use nom::Finish;
+use nom::IResult;
 
 use fimfareader::archive::Story;
 use fimfareader::error::*;
@@ -20,6 +37,7 @@ pub enum Source {
     DtuFn(Box<dyn Fn(&Story) -> &Option<DateTime<Utc>> + Sync>),
 }
 
+#[derive(Clone)]
 pub enum Operator {
     Exact,
     Fuzzy,
@@ -28,67 +46,79 @@ pub enum Operator {
 }
 
 macro_rules! sfn {
-    ($func:expr) => {
-        |_| Source::StrFn(Box::new($func))
+    ($tag:expr => $func:expr) => {
+        preceded(tag($tag), |input| {
+            Ok((input, Source::StrFn(Box::new($func))))
+        })
     };
 }
 
 macro_rules! ifn {
-    ($func:expr) => {
-        |_| Source::IntFn(Box::new($func))
-    };
+    ($tag:expr => $func:expr) => {{
+        preceded(tag($tag), |input| {
+            Ok((input, Source::IntFn(Box::new($func))))
+        })
+    }};
 }
 
 macro_rules! dfn {
-    ($func:expr) => {
-        |_| Source::DtuFn(Box::new($func))
+    ($tag:expr => $func:expr) => {
+        preceded(tag($tag), |input| {
+            Ok((input, Source::DtuFn(Box::new($func))))
+        })
     };
 }
 
-named!(source<&str, Source>, preceded!(space0, alt!(
-    tag!("id") => { ifn!(|s| s.id) } |
+fn source(input: &str) -> IResult<&str, Source> {
+    let story = alt((
+        ifn!("id" => |s| s.id),
+        sfn!("story" => |s| &s.title),
+        sfn!("title" => |s| &s.title),
+        sfn!("description" => |s| &s.description_html),
+        sfn!("short description" => |s| &s.short_description),
+        sfn!("url" => |s| &s.url),
+        dfn!("modified" => |s| &s.date_modified),
+        dfn!("published" => |s| &s.date_published),
+        dfn!("updated" => |s| &s.date_updated),
+        ifn!("chapters" => |s| i64::from(s.num_chapters)),
+        ifn!("comments" => |s| i64::from(s.num_comments)),
+        ifn!("dislikes" => |s| i64::from(s.num_dislikes)),
+        ifn!("likes" => |s| i64::from(s.num_likes)),
+        ifn!("total views" => |s| i64::from(s.total_num_views)),
+        ifn!("views" => |s| i64::from(s.num_views)),
+        ifn!("words" => |s| i64::from(s.num_words)),
+    ));
 
-    tag!("story") => { sfn!(|s| &s.title) } |
-    tag!("title") => { sfn!(|s| &s.title) } |
+    let author = alt((
+        sfn!("author" => |s| &s.author.name),
+        sfn!("author name" => |s| &s.author.name),
+        ifn!("author id" => |s| s.author.id),
+        dfn!("author joined" => |s| &s.author.date_joined),
+    ));
 
-    tag!("description") => { sfn!(|s| &s.description_html) } |
-    tag!("short description") => { sfn!(|s| &s.short_description) } |
-    tag!("url") => { sfn!(|s| &s.url) } |
+    let archive = alt((
+        sfn!("path" => |s| &s.archive.path),
+        sfn!("archive" => |s| &s.archive.path),
+        sfn!("archive path" => |s| &s.archive.path),
+        dfn!("entry checked" => |s| &s.archive.date_checked),
+        dfn!("entry created" => |s| &s.archive.date_created),
+        dfn!("entry fetched" => |s| &s.archive.date_fetched),
+        dfn!("entry updated" => |s| &s.archive.date_updated),
+    ));
 
-    tag!("modified") => { dfn!(|s| &s.date_modified) } |
-    tag!("published") => { dfn!(|s| &s.date_published) } |
-    tag!("updated") => { dfn!(|s| &s.date_updated) } |
+    preceded(space0, alt((story, author, archive)))(input)
+}
 
-    tag!("chapters") => { ifn!(|s| i64::from(s.num_chapters)) } |
-    tag!("comments") => { ifn!(|s| i64::from(s.num_comments)) } |
-    tag!("dislikes") => { ifn!(|s| i64::from(s.num_dislikes)) } |
-    tag!("likes") => { ifn!(|s| i64::from(s.num_likes)) } |
-    tag!("total views") => { ifn!(|s| i64::from(s.total_num_views)) } |
-    tag!("views") => { ifn!(|s| i64::from(s.num_views)) } |
-    tag!("words") => { ifn!(|s| i64::from(s.num_words)) } |
+fn operator(input: &str) -> IResult<&str, Operator> {
+    let operator = alt((
+        value(Operator::Exact, char('=')),
+        value(Operator::Fuzzy, char(':')),
+        value(Operator::LessThan, char('<')),
+        value(Operator::MoreThan, char('>')),
+    ));
 
-    tag!("author") => { sfn!(|s| &s.author.name) } |
-    tag!("author name") => { sfn!(|s| &s.author.name) } |
-
-    tag!("author id") => { ifn!(|s| s.author.id) } |
-    tag!("author joined") => { dfn!(|s| &s.author.date_joined) } |
-
-    tag!("path") => { sfn!(|s| &s.archive.path) } |
-    tag!("archive") => { sfn!(|s| &s.archive.path) } |
-    tag!("archive path") => { sfn!(|s| &s.archive.path) } |
-
-    tag!("entry checked") => { dfn!(|s| &s.archive.date_checked) } |
-    tag!("entry created") => { dfn!(|s| &s.archive.date_created) } |
-    tag!("entry fetched") => { dfn!(|s| &s.archive.date_fetched) } |
-    tag!("entry updated") => { dfn!(|s| &s.archive.date_updated) }
-)));
-
-named!(operator<&str, Operator>, preceded!(space0, alt!(
-    tag!("=") => { |_| Operator::Exact } |
-    tag!(":") => { |_| Operator::Fuzzy } |
-    tag!("<") => { |_| Operator::LessThan } |
-    tag!(">") => { |_| Operator::MoreThan }
-)));
+    preceded(space0, operator)(input)
+}
 
 fn unescape(input: &str) -> String {
     input
@@ -98,47 +128,50 @@ fn unescape(input: &str) -> String {
         .replace("\\\\", "\\")
 }
 
-named!(value<&str, &str>,
-    escaped!(none_of!("),|\\"), '\\', one_of!("),|\\"))
-);
+fn evalue(input: &str) -> IResult<&str, &str> {
+    escaped(none_of("),|\\"), '\\', one_of("),|\\"))(input)
+}
 
-named!(target<&str, String>, preceded!(space0,
-    map!(value, |value| unescape(value.trim()))
-));
+fn target(input: &str) -> IResult<&str, String> {
+    preceded(space0, map(evalue, |value| unescape(value.trim())))(input)
+}
 
 fn item(input: &str) -> IResult<&str, Filter> {
     let result = tuple((source, operator, target))(input)?;
     let (left, (src, op, value)) = result;
 
     let Ok(filter) = optimize(src, op, &value) else {
-        return Err(Err::Failure((input, NomErrorKind::Permutation)));
+        let error = NomError::new(input, NomErrorKind::Permutation);
+        return Err(NomErr::Failure(error));
     };
 
     Ok((left, filter))
 }
 
-named!(parens<&str, Filter>, alt!(
-    delimited!(
-        preceded!(space0, tag!("(")),
-        preceded!(space0, call!(ofunc)),
-        preceded!(space0, tag!(")"))
-    ) |
-    call!(item)
-));
+fn parens(input: &str) -> IResult<&str, Filter> {
+    let group = delimited(
+        preceded(space0, char('(')),
+        preceded(space0, ofunc),
+        preceded(space0, char(')')),
+    );
 
-fn negate(input: &str) -> IResult<&str, Filter> {
-    let (left, filter) = parens(input)?;
-
-    Ok((left, Box::new(move |s| !filter(s))))
+    alt((group, item))(input)
 }
 
-named!(nlist<&str, Filter>, preceded!(space0, alt!(
-    preceded!(char('!'), call!(negate)) | call!(parens)
-)));
+fn negate(input: &str) -> IResult<&str, Filter> {
+    let (input, filter) = parens(input)?;
+    Ok((input, Box::new(move |s| !filter(s))))
+}
 
-named!(alist<&str, Vec<Filter>>, separated_nonempty_list!(
-    preceded!(space0, char(',')), call!(nlist)
-));
+fn nlist(input: &str) -> IResult<&str, Filter> {
+    let negated = preceded(char('!'), negate);
+    preceded(space0, alt((negated, parens)))(input)
+}
+
+fn alist(input: &str) -> IResult<&str, Vec<Filter>> {
+    let sep = preceded(space0, char(','));
+    separated_list1(sep, nlist)(input)
+}
 
 fn afunc(input: &str) -> IResult<&str, Filter> {
     let (left, mut filters) = alist(input)?;
@@ -160,9 +193,10 @@ fn afunc(input: &str) -> IResult<&str, Filter> {
     Ok((left, filter))
 }
 
-named!(olist<&str, Vec<Filter>>, separated_nonempty_list!(
-    preceded!(space0, char('|')), call!(afunc)
-));
+fn olist(input: &str) -> IResult<&str, Vec<Filter>> {
+    let sep = preceded(space0, char('|'));
+    separated_list1(sep, afunc)(input)
+}
 
 fn ofunc(input: &str) -> IResult<&str, Filter> {
     let (left, mut filters) = olist(input)?;
@@ -184,33 +218,13 @@ fn ofunc(input: &str) -> IResult<&str, Filter> {
     Ok((left, filter))
 }
 
-fn format_error(query: &str, input: &str, error: NomErrorKind) -> String {
-    let description = error.description().to_lowercase();
-    let position = query.len() - input.len();
-
-    format!("Invalid {} at {}", description, position)
-}
-
-fn translate_error(query: &str, error: Err<(&str, NomErrorKind)>) -> Error {
-    let message = match error {
-        Err::Error((i, e)) => format_error(query, i, e),
-        Err::Failure((i, e)) => format_error(query, i, e),
-        Err::Incomplete(_) => String::from("Incomplete input"),
-    };
-
-    Error::query(message)
-}
-
-fn translate_incomplete(query: &str, input: &str) -> Error {
-    let position = query.len() - input.len();
-
-    Error::query(format!("Incomplete input at {}", position))
+fn complete(input: &str) -> IResult<&str, Filter> {
+    terminated(ofunc, eof)(input.trim())
 }
 
 pub fn parse(query: &str) -> Result<Filter> {
-    match ofunc(query.trim()) {
-        Ok(("", filter)) => Ok(filter),
-        Ok((i, _)) => Err(translate_incomplete(query, i)),
-        Err(e) => Err(translate_error(query, e)),
+    match complete(query).finish() {
+        Ok((_, filter)) => Ok(filter),
+        Err(e) => Err(Error::query(e)),
     }
 }
